41071c393ebf1198b87644ab7b9c06587d3af80b
Jack Harvey <jack.harvey1019@gmail.com>
2024-09-19 11:59:30 -0700

    started detect_face_contour() function

--- script.py
+++ script.py
@@ -2,6 +2,19 @@ import numpy as np
 import argparse
 import cv2
 
+# function to detect largest contour
+def detect_face_contour(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (11,11), 0)
+    edged = cv2.Canny(blurred, 30, 150)
+
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    face_contour = max(cnts, key=cv2.contourArea) # assuming largest contour is the face
+    # this may be a simplification (we will see as we input different images)
+
+    return face_contour
+
 # main code
 ap = argparse.ArgumentParser()
 ap.add_argument("-i1", "--image1", required=True, # for multiple images, use specific flags
